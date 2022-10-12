e1e57e07319a741491a7c2a367cc15a4d86f934c
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-10-12 17:47:26 +0300

    models of AirBnB_clone project

--- models/user.py
+++ models/user.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""Implements the user's model"""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+class User(BaseModel):
+    """
+    Inherits from the BaseModel class and add user's functionalities
+
+    Args:
+        email (str): the email of the user
+        password (str): the password of the user
+        first_name (str): the first name of the user
+        last_name (str): the last name of the user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
