9822597fcf402ba864785aaaa508e601683529e2
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-13 19:38:31 +0000

    a script that starts a Flask web application: with 3 routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
-"""Starts Flask web app
-Routes:
-    / - display "Hello HBNB!"
-    /hbnb - display "HBNB"
-    /c/<text> - display "C <text>"
+"""
+This script starts a Flask web application
+which listens on 0.0.0.0 and port 5000
+it has two routes hello_HBNB and HBNB
 """
 from flask import Flask
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb_route():
-    """prints Hello HBNB"""
-    return "Hello HBNB!"
+def hello_flask():
+    """
+    This route displays Hello HBNB!
+    """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """prints HBNB"""
+    '''
+    This page displays HBNB
+    '''
     return "HBNB"
 
 
-@app.route('/c/<string:text>', strict_slashes=False)
-def c_text(text):
-    """prints C followed by <text> content"""
-    text = text.replace("_", " ")
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    '''
+    Displays C followed by the value of text variable
+    '''
     return "C %s" % text
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
