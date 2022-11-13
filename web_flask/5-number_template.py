afc0a18df169a83ee8492d4d7a41f67cb924af22
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-13 20:20:31 +0000

    a script that starts a Flask web application: with 7 route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,56 +1,63 @@
 #!/usr/bin/python3
-"""Starts Flask web app
-Routes:
-    / - display "Hello HBNB!"
-    /hbnb - display "HBNB"
-    /c/<text> - display "C <text>"
-    /python/<text> - display "Python is cool"
-    /number/<n> - display n if integer
-    /number_template/<n> - display a HTML page if n is int
 """
-from flask import Flask, render_template
+This script starts a Flask web application
+which listens on 0.0.0.0 and port 5000
+it has two routes hello_HBNB and HBNB
+"""
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hbnb_route():
-    """prints Hello HBNB"""
-    return "Hello HBNB!"
+def hello_flask():
+    """
+    This route displays Hello HBNB!
+    """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """prints HBNB"""
+    '''
+    This page displays HBNB
+    '''
     return "HBNB"
 
 
 @app.route('/c/<string:text>', strict_slashes=False)
-def c_text(text):
-    """prints C followed by <text> content"""
+def c_is_fun(text):
+    '''
+    Displays C followed by the value of text variable
+    '''
     text = text.replace("_", " ")
-    return "C %s" % text
+    return 'C %s' % text
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
-def python_text(text="is cool"):
-    """prints Python is cool"""
+def python_is_cool(text="is cool"):
+    '''
+    Displays Python followed by the value of the text variable
+    '''
     text = text.replace("_", " ")
     return "Python %s" % text
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_n(n):
-    """displays n if integer"""
-    return "%i is a number" % n
+def is_number(n):
+    '''
+    Displays a number followed by the text " is a number"
+    '''
+    return '%d is a number' % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """display a HTML page if n is int"""
     return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
